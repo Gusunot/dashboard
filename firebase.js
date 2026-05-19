0969e8d20bdcfd2f3587f9f06b990dfdfd56638f
Gusunot <67029677+Gusunot@users.noreply.github.com>
2026-05-19 17:32:13 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Mb-GifYyYfB0r3Xz7cILoingPf1NP98",
+  authDomain: "dashboardprogb.firebaseapp.com",
+  projectId: "dashboardprogb",
+  storageBucket: "dashboardprogb.firebasestorage.app",
+  messagingSenderId: "738870080265",
+  appId: "1:738870080265:web:9c2201996213c7eb0f19d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
