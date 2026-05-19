19a449190c5d0d17cc39d1b03bb505a0fe05b4ca
Gusunot <67029677+Gusunot@users.noreply.github.com>
2026-05-19 17:40:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -601,3 +601,73 @@ onAuthStateChanged(auth,(user)=>{
     .style.display = 'flex';
   }
 });
+
+async function registrar(){
+
+  const email =
+  document.getElementById('email').value;
+
+  const senha =
+  document.getElementById('senha').value;
+
+  try{
+
+    await createUserWithEmailAndPassword(
+      auth,
+      email,
+      senha
+    );
+
+    mostrarToast('Conta criada com sucesso');
+
+  }catch(error){
+
+    alert(error.message);
+  }
+}
+
+window.registrar = registrar;
+
+async function login(){
+
+  const email =
+  document.getElementById('email').value;
+
+  const senha =
+  document.getElementById('senha').value;
+
+  try{
+
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      senha
+    );
+
+    mostrarToast('Login realizado');
+
+  }catch(error){
+
+    alert(error.message);
+  }
+}
+
+window.login = login;
+
+onAuthStateChanged(auth,(user)=>{
+
+  if(user){
+
+    document
+    .getElementById('loginTela')
+    .style.display = 'none';
+
+    carregarDadosFirestore(user.uid);
+
+  }else{
+
+    document
+    .getElementById('loginTela')
+    .style.display = 'flex';
+  }
+});
